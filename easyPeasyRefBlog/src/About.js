d98d2056e35654cae8317058b42e6248ebcaad23
Damoke01 <okedamola01@gmail.com>
2023-09-04 12:14:48 +0100

    Moving forward

---
+++ easyPeasyRefBlog/src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+      <main className="About"> 
+          <h2>About</h2>
+          <p style={{ marginTop: '1rem' }}>
+            This blog app is a project i built while learning react from Dave Gray's tutorial series.
+          </p>
+      </main>
+    )
+  }
+  
+  export default About
